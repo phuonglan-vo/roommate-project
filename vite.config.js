51c0644246f17981b933479bc7c4c1b9d98016c4
vplan <phuonglan060295@gmail.com>
2026-08-04 00:43:05 +0700

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,30 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => ({
-  /*
-   * npm run dev:
-   * http://127.0.0.1:5173/
-   *
-   * npm run build và npm run preview:
-   * /roommate-project/
-   */
-  base:
-    mode === 'development'
+export default defineConfig(({ mode }) => {
+  const base =
+    process.env.BASE_PATH ??
+    (mode === 'development'
       ? '/'
-      : '/roommate-project/',
+      : '/roommate-project/');
 
-  server: {
-    host: '127.0.0.1',
-    port: 5173,
-    strictPort: true
-  },
+  return {
+    base,
 
-  preview: {
-    host: '127.0.0.1',
-    port: 4174,
-    strictPort: true
-  },
+    server: {
+      host: '127.0.0.1',
+      port: 5173,
+      strictPort: true
+    },
 
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
-}));
+    preview: {
+      host: '127.0.0.1',
+      port: 4174,
+      strictPort: true
+    },
+
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true
+    }
+  };
+});
